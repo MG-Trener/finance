bedea7b28b56c33db4ebb58c5035e56d2152df88
MG-Trener <mihagavr@gmail.com>
2026-09-06 18:44:35 +0500

    Keep wife dictionary modal at top

--- src/js/ui/modal-stability.js
+++ src/js/ui/modal-stability.js
@@ -15,6 +15,8 @@
     }else if(modal.querySelector('#editNotice')){
       modal.classList.add('transaction-edit-modal');
       backdrop.classList.add('modal-layout-top');
+    }else if(modal.classList.contains('wife-dictionary-modal')||modal.querySelector('#wifeDictionaryForm')){
+      backdrop.classList.add('modal-layout-top');
     }else if(modal.classList.contains('transfer-modal')||modal.querySelector('#transferForm')){
       modal.classList.add('transfer-modal');
       backdrop.classList.add('modal-layout-center');
