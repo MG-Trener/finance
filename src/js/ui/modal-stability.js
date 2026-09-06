e7e082584675fd25c43ff495b810f8a4ad144966
MG-Trener <mihagavr@gmail.com>
2026-09-06 18:57:43 +0500

    Center dictionary edit modal

--- src/js/ui/modal-stability.js
+++ src/js/ui/modal-stability.js
@@ -15,6 +15,8 @@
     }else if(modal.querySelector('#editNotice')){
       modal.classList.add('transaction-edit-modal');
       backdrop.classList.add('modal-layout-top');
+    }else if(modal.classList.contains('wife-dictionary-edit-modal')||modal.querySelector('#wifeDictionaryEditForm')){
+      backdrop.classList.add('modal-layout-center');
     }else if(modal.classList.contains('wife-dictionary-modal')||modal.querySelector('#wifeDictionaryForm')){
       backdrop.classList.add('modal-layout-top');
     }else if(modal.classList.contains('transfer-modal')||modal.querySelector('#transferForm')){
